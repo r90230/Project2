970e49df464efd00732ac1e582c5e8ad0dbf5840
John Hereshko <JayHereshko@fullsail.edu>
2014-01-14 18:54:07 -0500

    Update HereshkoJohn_assignment2.js
    Added variables and templated prompts.

--- HereshkoJohn_assignment2.js
+++ HereshkoJohn_assignment2.js
@@ -3,3 +3,17 @@ John Hereshko
 SDI 1401
 Code for Project 2
 */
+
+// Variables
+var myHealth = 100
+var myDungeon = "Black Mist"
+var myKnight = true
+
+// Prompt for input
+myName = prompt("What is your name, Knight?","Lancelot");
+
+if (myName === Lancelot){
+console.log("Hello there, Sir Lancelot!")
+} else {
+console.log("Oh...from a distance, you looked like Sir Lancelot. Ahh, never you mind.")
+};
