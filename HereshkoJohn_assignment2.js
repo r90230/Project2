5b93ec8322a65bc44cdb5121dc7eea274f8b7b02
John Hereshko <JayHereshko@fullsail.edu>
2014-01-16 12:09:34 -0500

    Update HereshkoJohn_assignment2.js
    Added confirm, Boolean function, While loop

--- HereshkoJohn_assignment2.js
+++ HereshkoJohn_assignment2.js
@@ -5,21 +5,44 @@ Code for Project 2
 */
 
 // Variables
-var myHealth = 100
-var myDungeon = "Black Mist"
-var myKnight = true
+var myHealth = 100;
+var myDungeon = "Black Mist";
+var myWeapon = true;
+var myArrows = 20;
+var mySharpness = 20;
+var dragonNorth = true;
+var mySteps = 30
 
 // Prompt for input
 myName = prompt("What is your name, Knight?","Lancelot")
 
-var proclaimName = function(name){
-  if (name = "Lancelot"){ // Prompt Argument
-    console.log("Hello there, Knight Lancelot! We have quite the adventure planned for you today!")
-  } else {
-    console.log("Hmm...from a distance you looked very similar to Knight Lancelot...Ahh, never you mind.");
+var proclaimName = function(name){ // Prompt Argument
+  if (name === "Lancelot"){  // Argument conditional
+    console.log("Hello there, Knight Lancelot! We have quite the adventure planned for you today!") // Output 1
+  } else if (name != "Lancelot") {
+    console.log("Hmm...from a distance you looked very similar to Knight Lancelot...Ahh, never you mind."); // Output 2
+    return name
   };
 };
 
-proclaimName(myName);
+proclaimName(myName); // Running function for Procedure
 
-//Adding in 
+dragonNorth = confirm("The dragon appears to be heading to the north. Ok to head north, or Cancel to head south")
+// Confirm for direction selection
+
+var goingNorth = function(direction, steps) {
+	if (direction === true && steps > 0){
+		console.log("I am going north to find the dragon!")
+	} else if (dragonNorth === false){
+		console.log("I am not pursuing the dragon!")
+	};
+	}; // Creating Boolean function
+	
+while (mySteps > 0){
+	goingNorth
+	console.log("I am " + mySteps + " away from the dragon!")
+	mySteps--
+}; // While loop for steps
+
+console.log("I have reached the dragon!")
+
